57fd63c8499d9e6503d1adced7349411aeabce30
josesaranda <josesaranda@gmail.com>
2023-01-16 12:31:10 +0100

    Improvements

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -2,17 +2,23 @@ import { useState, useEffect } from "react";
 import "./ThemeButton.css";
 
 export const ThemeButton = () => {
+  const THEME_TOKEN = "theme";
   const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
     const preferredDarkMode =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
+      window.localStorage.getItem(THEME_TOKEN) === "dark" ||
+      (window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
     setIsDark(preferredDarkMode);
 
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addEventListener("change", (event) => {
+        window.localStorage.setItem(
+          THEME_TOKEN,
+          event.matches ? "dark" : "light"
+        );
         setIsDark(event.matches);
       });
   }, []);
@@ -24,7 +30,15 @@ export const ThemeButton = () => {
   }, [isDark]);
 
   return (
-    <button className="theme-button" onClick={() => setIsDark(!isDark)}>
+    <button
+      className="theme-button"
+      onClick={() => {
+        setIsDark((isDark) => {
+          window.localStorage.setItem(THEME_TOKEN, !isDark ? "dark" : "light");
+          return !isDark;
+        });
+      }}
+    >
       {isDark ? "🌞" : "🌙"}
     </button>
   );
