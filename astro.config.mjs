8d3e9971179ad472b601b6e6d2469e63758dfbc7
josesaranda <josesaranda@gmail.com>
2023-06-10 14:45:16 +0200

    Improvements

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
-import react from "@astrojs/react";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [astroI18next(), react()]
-});
