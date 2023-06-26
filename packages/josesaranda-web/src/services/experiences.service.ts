d4d8c57a78080f8174bdeb8f78c49c698be2e173
josesaranda <josesaranda@gmail.com>
2023-06-26 16:28:13 +0200

    Add knowmad mood experience

--- packages/josesaranda-web/src/services/experiences.service.ts
+++ packages/josesaranda-web/src/services/experiences.service.ts
@@ -1,9 +1,10 @@
 export function getExperiences(): string[] {
   return [
+    "knowmadmood",
     "fortris",
     "shapelets",
     "sombradoble",
     "nowait",
     "sombradoble-intern",
-  ]
+  ];
 }
