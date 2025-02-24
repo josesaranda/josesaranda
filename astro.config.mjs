a7b284ee21c55af1da2dcb6966e8382fd8bc658b
josesaranda <josesaranda@gmail.com>
2025-02-24 15:51:28 +0100

    feat: update project structure and remove unused components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [tailwind()],
 });
